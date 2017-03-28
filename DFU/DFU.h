cbb6a11cc9ea2373da3bbafbe2a9a9a518d85e1d
Arvid Klang <arvid@klang.us>
2017-03-28 11:19:37 +0200

    add quick and dirty xcode project with framework target that can be included in other xcworkspaces

---
+++ DFU/DFU.h
@@ -0,0 +1,19 @@
+//
+//  DFU.h
+//  DFU
+//
+//  Created by Arvid Klang on 28.3.2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DFU.
+FOUNDATION_EXPORT double DFUVersionNumber;
+
+//! Project version string for DFU.
+FOUNDATION_EXPORT const unsigned char DFUVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DFU/PublicHeader.h>
+
+
